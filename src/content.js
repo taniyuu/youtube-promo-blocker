adc8de353aef18cf615de7fef5d0b36a755faaba
谷山雄基 <cmt.0801nonki@gmail.com>
2020-03-29 11:21:49 +0900

    同期関数に戻す

--- src/content.js
+++ src/content.js
@@ -4,36 +4,13 @@ import store from './store';
 const url = 'https://www.cas.go.jp/jp/influenza/novel_coronavirus.html'
 searchAndHidden(url)
 
-function sleep(time) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-        resolve();
-        }, time);
-    });
-}
-
-async function domSearchWithRetry(parent, selector, counter = 0) {
-    const MAX_RETRY_COUNT = 10;
-    const result = parent.querySelector(selector);
-    if (!result) {
-        if(counter >= MAX_RETRY_COUNT) {
-            return null;
-        }
-        else {
-            await sleep(200);
-            return domSearchWithRetry(parent, selector, counter++);
-        }
-    }
-    return result;
-}
-
-async function searchAndHidden(url) {
-    const parent = await domSearchWithRetry(document, "#clarify-box");
+function searchAndHidden(url) {
+    const parent = document.querySelector("#clarify-box");
     if (!parent) {
         console.warn('親要素なし');
         return;
     }
-    const dom = await domSearchWithRetry(parent, ".yt-simple-endpoint.style-scope.ytd-clarification-renderer");
+    const dom = parent.querySelector(".yt-simple-endpoint.style-scope.ytd-clarification-renderer");
     if (!dom) {
         console.warn('子要素なし');
         return;
@@ -52,18 +29,10 @@ async function searchAndHidden(url) {
     return;
 }
 
-chrome.runtime.onMessage.addListener(async function(request, sender, sendResponse){
+chrome.runtime.onMessage.addListener(function(_request, _sender, sendResponse){
     let selection;
+    // FIXME store経由か、ローカルストレージから取得
     console.error(url);
-    const result = await searchAndHidden(url);
-    // console.error(request.message); // -> 選択範囲ちょうだい が出力される
-    
-    // 画面で選択されている部分を文字列で取得する
-    /*if(window.getSelection){
-      selection = window.getSelection().toString();
-    }else{
-      selection = '';
-    }*/
-    console.error(result);
+    const result = searchAndHidden(url);
     sendResponse(!!result);
   });
