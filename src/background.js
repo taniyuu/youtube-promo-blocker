1cbae0ef1b5dbe06e27779d7fd7ca33428133457
谷山雄基 <cmt.0801nonki@gmail.com>
2020-03-29 14:12:43 +0900

    アイコン制御完了

--- src/background.js
+++ src/background.js
@@ -6,18 +6,42 @@ const YOUTUBE = 'https://www.youtube.com/';
 const PATH_GRAY="icons/gray_48.png";
 const PATH_BLUE="icons/blue_48.png";
 
-function sendMessage(tabId) {
-    chrome.tabs.sendMessage(tabId, {url}, function(item){
-        console.error('hogeeeee')
-        console.error(item);
-        chrome.browserAction.setIcon({path:PATH_BLUE});
+function sleep(time) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+        resolve();
+        }, time);
+    });
+}
+
+function sendMessage(tab, counter = 0) {
+    const MAX_RETRY_COUNT = 10;
+    if(counter >= MAX_RETRY_COUNT) {
+        console.info('not found');
+        chrome.browserAction.setIcon({path:PATH_GRAY});
+        return;
+    }
+    chrome.tabs.sendMessage(tab.id, {url,counter}, async function(item){
+        console.info(item);
+        if(!item){
+            // サポート外のパスの場合null
+            chrome.browserAction.setIcon({path:PATH_GRAY});
+            return;
+        }
+        else{
+            await sleep(200);
+            const incCount =item.counter+1;
+            if(item.result) chrome.browserAction.setIcon({path:PATH_BLUE});
+            else chrome.browserAction.setIcon({path:PATH_GRAY})
+            sendMessage(tab, incCount);
+        }
     });
 }
 
 chrome.tabs.onActivated.addListener(function(activeInfo){
     chrome.tabs.get(activeInfo.tabId,function(tab){
         if(tab.url.indexOf(YOUTUBE) !== -1) {
-            sendMessage(tab.id);
+            sendMessage(tab);
         }else{
             chrome.browserAction.setIcon({path:PATH_GRAY});
         }
@@ -25,7 +49,7 @@ chrome.tabs.onActivated.addListener(function(activeInfo){
 })
 
 chrome.tabs.onUpdated.addListener(function(_tabid, info, tab){
-    if (info.status === 'complete' && tab.url.indexOf(YOUTUBE) !== -1) {
-        sendMessage(tab.id);
+    if (tab.url.indexOf(YOUTUBE) !== -1 && info.status === 'complete') {
+        sendMessage(tab);
     }
 })
--- src/content.js
+++ src/content.js
@@ -2,26 +2,24 @@ import store from './store';
 //alert(store.getters.foo);
 
 const url = 'https://www.cas.go.jp/jp/influenza/novel_coronavirus.html'
-searchAndHidden(url)
+//main();
 
-function searchAndHidden(url) {
-    const parent = document.querySelector("#clarify-box");
+function searchAndHidden(url, parentSelector, childSelector) {
+    const parent = document.querySelector(parentSelector);
     if (!parent) {
-        console.warn('親要素なし');
+        // console.warn('親要素なし');
         return;
     }
-    const dom = parent.querySelector(".yt-simple-endpoint.style-scope.ytd-clarification-renderer");
-    if (!dom) {
-        console.warn('子要素なし');
-        return;
-    }
-    const attrs = dom.attributes;
+    const nodeList = parent.querySelectorAll(childSelector);
     // 特殊なオブジェクトなのでforしか使えない
-    for(var i = attrs.length - 1; i >= 0; i--) {
-        if(attrs[i].name === 'href') {
-            if(attrs[i].value === url) {
-                parent.style.display ="none";
-                return true;
+    for(var j = nodeList.length - 1; j >= 0; j--) {
+        const attrs = nodeList[j].attributes;
+        for(var i = attrs.length - 1; i >= 0; i--) {
+            if(attrs[i].name === 'href') {
+                if(attrs[i].value === url) {
+                    parent.style.display ="none";
+                    return true;
+                }
             }
         }
     }
@@ -29,10 +27,34 @@ function searchAndHidden(url) {
     return;
 }
 
-chrome.runtime.onMessage.addListener(function(_request, _sender, sendResponse){
+function main(counter){
+    let responseObj = null;
+    const href = location.href;
+    switch (location.pathname) {
+        case '/':
+            responseObj = {
+                result:!!searchAndHidden(url, ".style-scope.ytd-popup-container", ".yt-simple-endpoint.style-scope.ytd-button-renderer"),
+                counter,
+                href
+            }
+            break;
+        case '/watch':
+            responseObj = {
+                result:!!searchAndHidden(url, "#clarify-box", ".yt-simple-endpoint.style-scope.ytd-clarification-renderer"),
+                counter,
+                href
+            }
+            break;
+        default:
+            // NOP
+    }
+    return responseObj;
+}
+
+chrome.runtime.onMessage.addListener(function(request, _sender, sendResponse){
     let selection;
     // FIXME store経由か、ローカルストレージから取得
-    console.error(url);
-    const result = searchAndHidden(url);
-    sendResponse(!!result);
+    // console.info(url);
+    let responseObj = main(request.counter);
+    sendResponse(responseObj);
   });
